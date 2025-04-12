7fe32c11bdfd5c16aa8ad2ca7889d53c2681e928
Raghav khandelwal <raghavkhandelwal483@gmail.com>
2025-04-13 02:05:50 +0530

    config setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allows external access (equivalent to --host)
+    allowedHosts: ['netflix-gpt-4.onrender.com'] // allow Render's domain
+  }
 });
